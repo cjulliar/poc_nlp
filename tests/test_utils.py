0bc5ad45b916d96b6833f0e70aa806e2e613726f
cjulliar <cyrjulliard@gmail.com>
2024-06-12 15:10:54 +0200

    ajout des tests - cas error

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -7,7 +7,7 @@ class TestUtils(unittest.TestCase):
     # Test la connexion a AzureChatOpenAI et AzureOpenAIEmbeddings
     @patch('app.utils.AzureChatOpenAI')
     @patch('app.utils.AzureOpenAIEmbeddings')
-    def test_initialize_llm_and_embedding(self, mock_embeddings, mock_llm):
+    def test_initialize_llm_and_embedding_success(self, mock_embeddings, mock_llm):
         mock_llm_instance = MagicMock()
         mock_embeddings_instance = MagicMock()
         mock_llm.return_value = mock_llm_instance
@@ -20,13 +20,24 @@ class TestUtils(unittest.TestCase):
         mock_llm.assert_called_once()
         mock_embeddings.assert_called_once()
 
+    # Test d'erreur avec AzureChatOpenAI et AzureOpenAIEmbeddings
+    @patch('app.utils.AzureChatOpenAI')
+    @patch('app.utils.AzureOpenAIEmbeddings')
+    def test_initialize_llm_and_embedding_error(self, mock_embeddings, mock_llm):
+        mock_llm.side_effect = Exception("LLM Error")
+        mock_embeddings.side_effect = Exception("Embedding Error")
+
+        with self.assertRaises(Exception):
+            utils.initialize_llm_and_embedding()
+
+
     # Test le chargement de WikipediaLoader
     # Test sur la division du document par RecursiveCharacterTextSplitter
     # Test le retriever retourne par Chroma
     @patch('app.utils.WikipediaLoader')
     @patch('app.utils.RecursiveCharacterTextSplitter')
     @patch('app.utils.Chroma')
-    def test_preprocess_wikipedia(self, mock_chroma, mock_text_splitter, mock_wikipedia_loader):
+    def test_preprocess_wikipedia_success(self, mock_chroma, mock_text_splitter, mock_wikipedia_loader):
         mock_loader_instance = mock_wikipedia_loader.return_value
         mock_loader_instance.load.return_value = ["Mocked Documents"]
 
@@ -45,13 +56,24 @@ class TestUtils(unittest.TestCase):
         mock_text_splitter.assert_called_once_with(chunk_size=1000, chunk_overlap=200, add_start_index=True)
         mock_chroma.from_documents.assert_called_once_with(["Split Document 1", "Split Document 2"], "Mocked Embedding")
 
+    @patch('app.utils.WikipediaLoader')
+    @patch('app.utils.RecursiveCharacterTextSplitter')
+    @patch('app.utils.Chroma')
+    def test_preprocess_wikipedia_error(self, mock_chroma, mock_text_splitter, mock_wikipedia_loader):
+        mock_loader_instance = mock_wikipedia_loader.return_value
+        mock_loader_instance.load.side_effect = Exception("Wikipedia Error")
+
+        with self.assertRaises(Exception):
+            utils.preprocess_wikipedia("test_page", "Mocked Embedding")
+
+
     # Test le chargement de YoutubeLoader
     # Test sur la division du document par RecursiveCharacterTextSplitter
     # Test le retriever retourne par Chroma
     @patch('app.utils.YoutubeLoader')
     @patch('app.utils.RecursiveCharacterTextSplitter')
     @patch('app.utils.Chroma')
-    def test_preprocess_youtube(self, mock_chroma, mock_text_splitter, mock_youtube_loader):
+    def test_preprocess_youtube_success(self, mock_chroma, mock_text_splitter, mock_youtube_loader):
         mock_loader_instance = mock_youtube_loader.return_value
         mock_loader_instance.load.return_value = [MagicMock(page_content="Mocked Transcript")]
 
@@ -70,6 +92,17 @@ class TestUtils(unittest.TestCase):
         mock_text_splitter.assert_called_once_with(chunk_size=1000, chunk_overlap=200)
         mock_chroma.from_documents.assert_called_once_with(["Split Document 1", "Split Document 2"], "Mocked Embedding")
 
+    @patch('app.utils.YoutubeLoader')
+    @patch('app.utils.RecursiveCharacterTextSplitter')
+    @patch('app.utils.Chroma')
+    def test_preprocess_youtube_error(self, mock_chroma, mock_text_splitter, mock_youtube_loader):
+        mock_loader_instance = mock_youtube_loader.return_value
+        mock_loader_instance.load.side_effect = Exception("Youtube Error")
+
+        with self.assertRaises(Exception):
+            utils.preprocess_youtube("https://youtube.com/watch?v=test_id", "Mocked Embedding")
+
+
     # Test la connexion a create_history_aware_retriever, create_stuff_documents_chain et create_retrieval_chain
     # Test les prompts créé par ChatPromptTemplate.from_messages
     # Test que l'output soit une instance de RunnableWithMessageHistory
@@ -77,11 +110,10 @@ class TestUtils(unittest.TestCase):
     @patch('app.utils.create_stuff_documents_chain')
     @patch('app.utils.create_retrieval_chain')
     @patch('app.utils.ChatPromptTemplate.from_messages')
-    def test_create_prompts_and_chains(self, mock_chat_prompt_template, mock_create_retrieval_chain, mock_create_stuff_documents_chain, mock_create_history_aware_retriever):
+    def test_create_prompts_and_chains_success(self, mock_chat_prompt_template, mock_create_retrieval_chain, mock_create_stuff_documents_chain, mock_create_history_aware_retriever):
         mock_llm = MagicMock()
         mock_retriever = MagicMock()
 
-        # Créer des mocks distincts pour les retours de ChatPromptTemplate.from_messages()
         mock_contextualize_q_prompt = MagicMock()
         mock_qa_prompt = MagicMock()
         mock_chat_prompt_template.side_effect = [mock_contextualize_q_prompt, mock_qa_prompt]
@@ -103,11 +135,25 @@ class TestUtils(unittest.TestCase):
 
         self.assertIsInstance(conversational_rag_chain, utils.RunnableWithMessageHistory)
 
+    @patch('app.utils.create_history_aware_retriever')
+    @patch('app.utils.create_stuff_documents_chain')
+    @patch('app.utils.create_retrieval_chain')
+    @patch('app.utils.ChatPromptTemplate.from_messages')
+    def test_create_prompts_and_chains_error(self, mock_chat_prompt_template, mock_create_retrieval_chain, mock_create_stuff_documents_chain, mock_create_history_aware_retriever):
+        mock_llm = MagicMock()
+        mock_retriever = MagicMock()
+
+        mock_chat_prompt_template.side_effect = Exception("Prompt Error")
+
+        with self.assertRaises(Exception):
+            utils.create_prompts_and_chains(mock_llm, mock_retriever)
+
+
     # Test la connexion a create_prompts_and_chains et execute_rag_chain
     # Test que get_answer_llm retourne la réponse correcte
     @patch('app.utils.create_prompts_and_chains')
     @patch('app.utils.execute_rag_chain')
-    def test_get_answer_llm(self, mock_execute_rag_chain, mock_create_prompts_and_chains):
+    def test_get_answer_llm_success(self, mock_execute_rag_chain, mock_create_prompts_and_chains):
         mock_llm = MagicMock()
         mock_retriever = MagicMock()
         mock_conversational_rag_chain = MagicMock()
@@ -119,6 +165,17 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(answer, "Mocked Answer")
         mock_create_prompts_and_chains.assert_called_once_with(mock_llm, mock_retriever)
         mock_execute_rag_chain.assert_called_once_with(mock_conversational_rag_chain, "test_user", "test_query")
+
+    @patch('app.utils.create_prompts_and_chains')
+    @patch('app.utils.execute_rag_chain')
+    def test_get_answer_llm_error(self, mock_execute_rag_chain, mock_create_prompts_and_chains):
+        mock_llm = MagicMock()
+        mock_retriever = MagicMock()
+
+        mock_create_prompts_and_chains.side_effect = Exception("Chain Error")
+
+        with self.assertRaises(Exception):
+            utils.get_answer_llm("test_user", mock_retriever, "test_query", mock_llm)
 
 if __name__ == '__main__':
     unittest.main()
