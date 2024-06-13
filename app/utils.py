94c6635c81125a5c9101fe6fb8dd83dc1e70e4b5
SimonRqrt <simon.riquart@icloud.com>
2024-06-13 14:29:42 +0200

    up integration corpus gradio

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,7 @@
 import os
 import re
 import pymupdf
+import pinecone
 
 from langchain_openai import AzureChatOpenAI, AzureOpenAIEmbeddings
 from langchain.chains import create_history_aware_retriever, create_retrieval_chain
@@ -13,6 +14,7 @@ from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
 
 store = {}
 
@@ -29,7 +31,28 @@ def extract_text_from_pdf(pdf_path):
         text += page.get_text()
     return text
 
-def initialize_llm_and_embedding():
+def extract_text_from_docx(doc_path):
+    try:
+        doc = Document(doc_path)
+        full_text = []
+        for paragraph in doc.paragraphs:
+            full_text.append(paragraph.text)
+        return "\n".join(full_text)
+    except Exception as e:
+        print(f"Error extracting text from DOCX file {doc_path}: {e}")
+        return ""
+
+def load_documents_from_directory(directory_path):
+    documents = []
+    for filename in os.listdir(directory_path):
+        file_path = os.path.join(directory_path, filename)
+        if filename.endswith(".pdf"):
+            documents.append(extract_text_from_pdf(file_path))
+        elif filename.endswith(".docx"):
+            documents.append(extract_text_from_docx(file_path))
+    return documents
+
+def initialize_llm_and_embedding_and_pinecone():
     llm = AzureChatOpenAI(
         azure_deployment=os.getenv("DEPLOYMENT_NAME_LLM"),
         openai_api_version="2023-06-01-preview",
@@ -40,7 +63,13 @@ def initialize_llm_and_embedding():
         azure_deployment=os.getenv("DEPLOYMENT_NAME_EMBEDDING"),
         openai_api_version="2023-05-15",
     )
-    return llm, embedding
+
+    pinecone_api_key = os.getenv("PINECONE_API_KEY")
+    pinecone_client = pinecone.Pinecone(
+        api_key=pinecone_api_key
+    )
+
+    return llm, embedding, pinecone_client
 
 def preprocess_wikipedia(page_name, embedding):
     docs = WikipediaLoader(query=page_name, load_max_docs=1, doc_content_chars_max=10000).load()
@@ -88,6 +117,21 @@ def preprocess_pdf(pdf_path, embedding):
     doc_splits = text_splitter.split_documents(docs)
     vectorstore = Chroma.from_documents(doc_splits, embedding)
     retriever = vectorstore.as_retriever()
+    return retriever
+
+def preprocess_corpus(directory_path, embedding):
+    corpus = load_documents_from_directory(directory_path)
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000, chunk_overlap=200, add_start_index=True
+    )
+    doc_splits = []
+    for text in corpus:
+        texts = text_splitter.split_text(text)
+        docs = [Document(page_content=t) for t in texts]
+        doc_splits.extend(text_splitter.split_documents(docs))
+    index_name = "corpus"
+    pinecone = PineconeVectorStore.from_documents(doc_splits,embedding,index_name)
+    retriever = pinecone.as_retriever()
     return retriever
 
 def create_prompts_and_chains(llm, retriever):
